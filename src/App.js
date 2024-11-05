dd6d9d948cc444878128ab1426dbe58ac09fd648
PierrePivz <chillingd@mbp-de-pierre.home>
2024-11-05 19:54:33 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Nav from './Nav.js';
-import { React, useState } from "react";
-import { Link , Router , Route } from "react-router";
-
-import './Pages.css';
-import Annexe from "./annexe.js";
-import Page from './Page.js';
 import Contact from "./Contact.js";
-
+import Manga_test from "./Manga/Manga_test.js";
 
 
 
 function App() {
 
-  const [filter, setFilter] = useState('');
-  
-  var test = document.querySelector(".uploader");
-  
-  function page(newselect,oldselect){
-
-    
-    test.classList.remove(oldselect);
-    test.classList.add(newselect);
-
-  }
-
- /*document.addEventListener( "onchange" , page(oldselect,newselect) );*/
-
 
   return (
     <div className="App">
         
-  <Nav />
-        
-        
-        
-        
-        <Page/>
 
-        <Contact/>
+        <Manga_test/>
+        
+         <Contact/>
 
       
     </div>
---
+++ src/Manga/Chapitre1.js
@@ -0,0 +1,71 @@
+import image1 from "../images/page1.jpg";
+import image2 from "../images/page2.jpg";
+import image3 from "../images/page3.jpg";
+import image4 from "../images/page4.jpg";
+import image5 from "../images/page5.jpg";
+import image6 from "../images/page6.jpg";
+import image7 from "../images/page7.jpg";
+import image8 from "../images/page8.jpg";
+import image9 from "../images/page9.jpg";
+import image10 from "../images/page10.jpg";
+import image11 from "../images/page11.jpg";
+import image12 from "../images/page12.jpg";
+import image13 from "../images/page14.jpg";
+import image14 from "../images/page15.jpg";
+import image15 from "../images/page16.jpg";
+import image16 from "../images/page17.jpg";
+import image17 from "../images/page18.jpg";
+import image18 from "../images/page19.jpg";
+import image19 from "../images/page20.jpg";
+import image20 from "../images/page21.jpg";
+import image21 from "../images/page22.jpg";
+import image22 from "../images/page23.jpg";
+import image23 from "../images/page24.jpg";
+import image24 from "../images/page25.jpg";
+import image25 from "../images/page26.jpg";
+import image26 from "../images/page27.jpg";
+import image27 from "../images/page28.jpg";
+import image28 from "../images/page29.jpg";
+import image29 from "../images/page30.jpg";
+import image30 from "../images/page31.jpg";
+import image31 from "../images/page32.jpg";
+import image32 from "../images/Page33.jpg";
+import image33 from "../images/page34.jpg";
+import image34 from "../images/page35.jpg";
+import image35 from "../images/page36.jpg";
+import image36 from "../images/page37.jpg";
+import image37 from "../images/page38.jpg";
+import image38 from "../images/page39.jpg";
+import image39 from "../images/page40.jpg";
+import image40 from "../images/page41.jpg";
+import image41 from "../images/page42.jpg";
+import image42 from "../images/page43.jpg";
+import image43 from "../images/page44.jpg";
+import image44 from "../images/page45.jpg";
+import image45 from "../images/page46.jpg";
+import image46 from "../images/page47.jpg";
+import image47 from "../images/page48.jpg";
+import image48 from "../images/page49.jpg";
+import image49 from "../images/page50.jpg";
+import image50 from "../images/page51.jpg";
+import image51 from "../images/page52.jpg";
+import image52 from "../images/page53.jpg";
+import image53 from "../images/page54.jpg";
+import image54 from "../images/page55.jpg";
+import image55 from "../images/page56.jpg";
+import image56 from "../images/page57.jpg";
+import image57 from "../images/page58.jpg";
+import image58 from "../images/page59.jpg";
+
+
+
+export default function chapitre(){
+
+const chapitre = [image1,image2,image3,image4,image5,image6,
+    image7,image8,image9,image10,image11,image12,image13,image14,
+    image15,image16,image17,image18,image19,image20,image21,image22,image23,image24,image25,image26,image27,image28,image29,image30,image31,image32,image33,image34,image35,image36,image37,image38,image39,
+    image40,image41,image42,image43,image44,image45,image46,image47,image48,image49,image50,image51,image52,image53,image54,image55,image56,image57,image58];
+
+    return chapitre;
+}
+
